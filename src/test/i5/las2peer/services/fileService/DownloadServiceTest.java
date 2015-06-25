8f679fe3854d2e7924f6166685075695c2dfa0a4
Thomas Cujé <tcuje@freenet.de>
2015-06-25 10:32:03 +0200

    resolved LAS-186 removed obsolete setSocketTimeout call, resolved warnings

--- src/test/i5/las2peer/services/fileService/DownloadServiceTest.java
+++ src/test/i5/las2peer/services/fileService/DownloadServiceTest.java
@@ -1,14 +1,11 @@
 package i5.las2peer.services.fileService;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import i5.las2peer.p2p.LocalNode;
 import i5.las2peer.security.ServiceAgent;
 import i5.las2peer.security.UserAgent;
 import i5.las2peer.testing.MockAgentFactory;
 import i5.las2peer.webConnector.WebConnector;
-import i5.las2peer.webConnector.client.ClientResponse;
 import i5.las2peer.webConnector.client.MiniClient;
 
 import java.io.ByteArrayOutputStream;
@@ -18,7 +15,6 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 /**
  * Example Test Class demonstrating a basic JUnit test structure.
  * 
@@ -27,22 +23,21 @@ import org.junit.Test;
  */
 public class DownloadServiceTest
 {
-	
+
 	private static final String HTTP_ADDRESS = "http://127.0.0.1";
 	private static final int HTTP_PORT = WebConnector.DEFAULT_HTTP_PORT;
-	
+
 	private static LocalNode node;
 	private static WebConnector connector;
 	private static ByteArrayOutputStream logStream;
-	
+
 	private static UserAgent testAgent;
 	private static final String testPass = "adamspass";
-	
+
 	private static final String testServiceClass = "i5.las2peer.services.fileService.DownloadService";
-	
-	private static final String mainPath = "files/";
-	
-	
+
+//	private static final String mainPath = "files/";
+
 	/**
 	 * Called before the tests start.
 	 * 
@@ -52,42 +47,39 @@ public class DownloadServiceTest
 	 */
 	@BeforeClass
 	public static void startServer() throws Exception {
-		
-		//start node
+
+		// start node
 		node = LocalNode.newNode();
 		node.storeAgent(MockAgentFactory.getAdam());
 		node.launch();
-		
+
 		ServiceAgent testService = ServiceAgent.generateNewAgent(testServiceClass, "a pass");
 		testService.unlockPrivateKey("a pass");
-		
+
 		node.registerReceiver(testService);
-		
-		//start connector
-		logStream = new ByteArrayOutputStream ();
-		
-		connector = new WebConnector(true,HTTP_PORT,false,1000);
-		connector.setSocketTimeout(10000);
-		connector.setLogStream(new PrintStream (logStream));
-		connector.start ( node );
-        Thread.sleep(1000); //wait a second for the connector to become ready
+
+		// start connector
+		logStream = new ByteArrayOutputStream();
+
+		connector = new WebConnector(true, HTTP_PORT, false, 1000);
+		connector.setLogStream(new PrintStream(logStream));
+		connector.start(node);
+		Thread.sleep(1000); // wait a second for the connector to become ready
 		testAgent = MockAgentFactory.getAdam();
-		
-        connector.updateServiceList();
-        //avoid timing errors: wait for the repository manager to get all services before continuing
-        try
-        {
-            System.out.println("waiting..");
-            Thread.sleep(10000);
-        }
-        catch(InterruptedException e)
-        {
-            e.printStackTrace();
-        }
-		
+
+		connector.updateServiceList();
+		// avoid timing errors: wait for the repository manager to get all services before continuing
+		try
+		{
+			System.out.println("waiting..");
+			Thread.sleep(10000);
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+
 	}
-	
-	
+
 	/**
 	 * Called after the tests have finished.
 	 * Shuts down the server and prints out the connector log file for reference.
@@ -95,46 +87,41 @@ public class DownloadServiceTest
 	 * @throws Exception
 	 */
 	@AfterClass
-	public static void shutDownServer () throws Exception {
-		
+	public static void shutDownServer() throws Exception {
+
 		connector.stop();
 		node.shutDown();
-		
-        connector = null;
-        node = null;
-        
-        LocalNode.reset();
-		
+
+		connector = null;
+		node = null;
+
+		LocalNode.reset();
+
 		System.out.println("Connector-Log:");
 		System.out.println("--------------");
-		
+
 		System.out.println(logStream.toString());
-		
-    }
-	
-	
+
+	}
 
 	@Test
 	public void testValidateLogin()
 	{
 		MiniClient c = new MiniClient();
 		c.setAddressPort(HTTP_ADDRESS, HTTP_PORT);
-		
+
 		try
 		{
 			c.setLogin(Long.toString(testAgent.getId()), testPass);
-            //ClientResponse result=c.sendRequest("GET", mainPath +"validate", "");
-           // assertEquals(200, result.getHttpCode());
+			// ClientResponse result=c.sendRequest("GET", mainPath +"validate", "");
+			// assertEquals(200, result.getHttpCode());
 
-		}
-		catch(Exception e)
+		} catch (Exception e)
 		{
 			e.printStackTrace();
-			fail ( "Exception: " + e );
+			fail("Exception: " + e);
 		}
-		
-    }
-	
-	
+
+	}
 
 }
